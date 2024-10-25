4bdf12ec701d5d36698f093d5670f4129d8a8f30
Bhuvanesh Duvvuri <realbhuvaneshduvvuri@gmail.com>
2024-10-25 22:31:39 +0530

    Done

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Home from "./pages/Home.jsx"
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+const container = document.querySelector(".root");
+const root = createRoot(container);
+root.render(
+  <Router>
+    <Routes>
+      <Route
+        path="/"
+        element={<Home />}
+      />
+    </Routes>
+  </Router>
+);
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: "./src/index.jsx",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    open: true,
+    port: 5000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /.module.css$/,
+      },
+      {
+        test: /.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /.module.css$/,
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+};
