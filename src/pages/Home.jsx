7aaf1097ac94c9a367d35b8d8b593552fc3a265b
Bhuvanesh Duvvuri <realbhuvaneshduvvuri@gmail.com>
2024-11-18 06:21:40 +0530

    Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
+import React from "react";
 
-      import React from "react";
-
-      export default function Home() {
-      return (
-      <div>
+export default function Home() {
+  return (
+    <div>
       <h1>Home</h1>
-      </div>
-      )
-      }
-      
+    </div>
+  );
+}
